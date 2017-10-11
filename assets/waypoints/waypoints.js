083a92b22ff59f984971b028886d576cccfdf58d
cesarisern <cesarisern@gmail.com>
2017-10-11 16:22:13 +0100

    Scroll changes images
    - Prouduct image changes on scroll

--- assets/waypoints/waypoints.js
+++ assets/waypoints/waypoints.js
@@ -1,11 +1,23 @@
-var waypoint = new Waypoint({
-  element: document.getElementById('waypointtest'),
+var waypoint1 = new Waypoint({
+  element: document.getElementById('waypoint1'),
   handler: function(direction) {
     if (direction=="down") {
-    $( ".Desktop-product-image-block" ).css( "visibility", "hidden");
+    document.getElementById('Product-image-1').src='assets/img/Desktop-img-2.png';
   } else if (direction=="up") {
-    $( ".Desktop-product-image-block" ).css( "visibility", "visible");
+    document.getElementById('Product-image-1').src='assets/img/Desktop-img-1.png';
   }
-    console.log(direction);
+    console.log("waypoint1: "+direction);
   }
-})
+});
+
+var waypoint2 = new Waypoint({
+  element: document.getElementById('waypoint2'),
+  handler: function(direction) {
+    if (direction=="down") {
+    document.getElementById('Product-image-1').src='assets/img/Desktop-img-3.png';
+  } else if (direction=="up") {
+    document.getElementById('Product-image-1').src='assets/img/Desktop-img-2.png';
+  }
+    console.log("waypoint2: "+direction);
+  }
+});
