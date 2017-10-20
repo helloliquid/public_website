f67ff9095eeb72de66a50c40746653755653429d
cesarisern <cesarisern@gmail.com>
2017-10-20 12:21:38 +0100

    New contents
    - Added new content. Copys and images.
    - Added @2X images for mobile

--- assets/waypoints/waypoints.js
+++ assets/waypoints/waypoints.js
@@ -21,3 +21,15 @@ var waypoint2 = new Waypoint({
     console.log("waypoint2: "+direction);
   }
 });
+
+var waypoint3 = new Waypoint({
+  element: document.getElementById('waypoint3'),
+  handler: function(direction) {
+    if (direction=="down") {
+    document.getElementById('Product-image-1').src='assets/img/Desktop-img-4.png';
+  } else if (direction=="up") {
+    document.getElementById('Product-image-1').src='assets/img/Desktop-img-3.png';
+  }
+    console.log("waypoint3: "+direction);
+  }
+});
